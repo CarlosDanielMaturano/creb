f8b3dd09909516680cd21915849a2d25211df55a
CarlosDanielMaturano <carlosdanielmaturano@proton.me>
2024-02-14 10:07:38 -0300

    ignore-case, colorize and invert flags implemented

--- src/args.rs
+++ src/args.rs
@@ -1,8 +1,55 @@
 use clap::Parser;
+use std::io::{Error, ErrorKind};
 
 #[derive(Parser, Debug)]
 pub struct Args {
     pub pattern: String,
     pub file_path: std::path::PathBuf,
-    pub flags: String,
+    pub flags: Vec<String>,
+}
+
+pub struct ArgsOptions {
+    pub invert: bool,
+    pub colorize: bool,
+    pub line_numbers: bool,
+}
+
+fn parse_flags(flags: &Vec<String>) -> Vec<String> {
+    let parsed_flags = flags
+        .iter()
+        .flat_map(|flag| {
+            if flag.is_empty() {
+                return vec![String::new()];
+            }
+            if flag.matches("-").count() >= 2 {
+                return vec![flag[2..].to_string()];
+            }
+            return flag[1..]
+                .chars()
+                .map(String::from)
+                .collect();
+        })
+        .collect::<Vec<String>>();
+    parsed_flags
+}
+
+impl ArgsOptions {
+    pub fn from_flags(flags: Vec<String>) -> Result<ArgsOptions, Error> {
+        let mut options = ArgsOptions {
+            invert: false,
+            colorize: false,
+            line_numbers: false,
+        };
+
+        for flag in parse_flags(&flags) {
+            match flag.as_str() {
+                "v" | "invert" => options.invert = true,
+                "c" | "color" => options.colorize = true,
+                "n" | "numbers" => options.line_numbers = true,
+                _ => return Err(Error::new(ErrorKind::Other, format!("Unknow flag: {flag}"))),
+            }
+        }
+
+        Ok(options)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 pub mod args;
 pub mod tokens;
-use args::Args;
+use args::{Args, ArgsOptions};
 use clap::Parser;
 
 fn read_file_content(path: &std::path::PathBuf) -> Result<String, std::io::Error> {
@@ -8,43 +8,59 @@ fn read_file_content(path: &std::path::PathBuf) -> Result<String, std::io::Error
     Ok(file_content)
 }
 
-fn match_flags(flags: &str) {
-    dbg!("{}", flags);
-    match flags {
-        "-v" => {}
-        _ => {}
-    }
+fn print_line_number(index: usize) {
+    print!("{: <8}", format!("{index}."));
 }
 
-fn print_match_content(pattern: String, content: String, colorize: bool) {
+fn print_match_content(pattern: String, content: String, options: ArgsOptions) {
     content.lines().enumerate().for_each(|(index, line)| {
         if !line.contains(&pattern) {
             return;
         }
+        if options.line_numbers {
+            print_line_number(index)
+        }
         // each token is a sequential character string without any whitspace that has been
         // found on the current line
         let tokens = tokens::split_line_tokens(line.to_string()).into_iter();
-
-        print!("{: <8}", format!("{index}."));
         // print the token list as a new line
         tokens.for_each(|token| {
             let mut token = token;
-            if !token.contains(&pattern) {
-                return
-            }
-            if colorize {
+            if options.colorize && token.contains(&pattern) {
                 token = tokens::colorize_token_with_pattern(&pattern, &token);
             }
             print!("{token}");
         });
         println!()
-    })
+    });
+}
+
+fn print_invert_content(pattern: String, content: String, options: ArgsOptions) {
+    content.lines().enumerate().for_each(|(index, line)| {
+        if line.contains(&pattern) {
+            return;
+        }
+        if options.line_numbers {
+            print_line_number(index);
+        }
+        println!("{line}");
+    });
 }
 
 fn main() {
     let args: Args = Args::parse();
+
+    let options = match ArgsOptions::from_flags(args.flags) {
+        Ok(options) => options,
+        Err(err) => {
+            println!("A error ocurred while parsing the flags: {err}");
+            std::process::exit(0)
+        }
+    };
     if let Ok(file_content) = read_file_content(&args.file_path) {
-        match_flags(&args.flags);
-        print_match_content(args.pattern, file_content, true);
+        if options.invert {
+            return print_invert_content(args.pattern, file_content, options);
+        }
+        print_match_content(args.pattern, file_content, options);
     }
 }
